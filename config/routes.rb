7c26752489d640ad7772157bcce0f727ab88297b
Brock Haugen <brock.haugen@icloud.com>
2015-03-04 10:39:48 -0800

    made task#index the default page and updated the _form.html.erb in views/tasks to pull the project names into a select tag

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'projects#index'
+  root 'tasks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
